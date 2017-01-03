3fbc0d16563f54b79222fd7436a722753f83d90c
prashant.s <prashant.s@flipkart.com>
2017-01-03 11:31:45 +0530

    KMeans with Tests

--- tests/kmeans_tests.py
+++ tests/kmeans_tests.py
@@ -1,6 +1,6 @@
 
 import nose.tools as nt  # contains testing tools like ok_, eq_, etc.
-import NAME
+import kmeans.kmeans
 
 def setup():
     print "SETUP!"
@@ -10,3 +10,25 @@ def teardown():
 
 def test_basic():
     print "I RAN!"
+
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def testDistaceFromCentroids():
+    initK = 10
+    (centroids, iters, points) = kmeans.kmeans.kmeansAlgo()
+    for i in range(len(points)):
+        cluster = points[i].getCluster()
+        centroid = centroids[cluster]
+        internalDist = kmeans.kmeans.get_distance(centroid.getX(),centroid.getY(), points[i].getX(),points[i].getY())
+        for k in range(len(centroids)):
+            if k != cluster:
+                centroid=centroids[k]
+                #distance of point to it's own centroid should be less than all other centroids
+                assert (kmeans.kmeans.get_distance(centroid.getX(),centroid.getY(), points[i].getX(),points[i].getY()) >= internalDist)
+
+
